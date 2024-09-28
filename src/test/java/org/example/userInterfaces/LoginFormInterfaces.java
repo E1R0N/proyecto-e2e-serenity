08bbb9fc9f80c311ca94fe9f7a643a91d74dd86f
E1R0N <mattayupanta@gmail.com>
2024-09-28 11:11:49 -0500

    first commit

---
+++ src/test/java/org/example/userInterfaces/LoginFormInterfaces.java
@@ -0,0 +1,9 @@
+package org.example.userInterfaces;
+
+import org.openqa.selenium.By;
+
+public class LoginFormInterfaces {
+    public static By USER_NAME_FIELD = By.id("user-name");
+    public static By PASSWORD_FIELD = By.id("password");
+    public static By LOGIN_BUTTON = By.id("login-button");
+}
