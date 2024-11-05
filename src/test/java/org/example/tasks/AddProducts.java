192d02ec9a29cd14adf6349badb8c5a8830cc9d3
E1R0N <mattayupanta@gmail.com>
2024-11-05 11:23:41 -0500

    Ranbdom product select

--- src/test/java/org/example/tasks/AddProducts.java
+++ src/test/java/org/example/tasks/AddProducts.java
@@ -5,26 +5,45 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 import org.example.userInterfaces.PurchaseProductsInterfaces;
+import org.openqa.selenium.By;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Arrays;
+
 public class AddProducts implements Task {
+
+    private static final List<By> PRODUCT_BUTTONS = Arrays.asList(
+            PurchaseProductsInterfaces.BACKPACK_BUTTON,
+            PurchaseProductsInterfaces.BIKE_LIGHTS_BUTTON,
+            PurchaseProductsInterfaces.FLEECE_JACKET_BUTTON,
+            PurchaseProductsInterfaces.BOLT_TSHIRT_BUTTON,
+            PurchaseProductsInterfaces.ONESIE_BUTTON,
+            PurchaseProductsInterfaces.TSHIRT_RED_BUTTON
+    );
 
     public static Task addProductsToCart(){
         return instrumented(AddProducts.class);
     }
 
     @Override
-    public <T extends Actor> void performAs(T actor){
+    public <T extends Actor> void performAs(T actor) {
+        Random random = new Random();
+
+        for (int i = 0; i < 2; i++) {
+            int randomIndex = random.nextInt(PRODUCT_BUTTONS.size());
+            By selectedProductButton = PRODUCT_BUTTONS.get(randomIndex);
+
+            actor.attemptsTo(
+                    WaitUntil.the(selectedProductButton, isVisible()).forNoMoreThan(20).seconds(),
+                    Click.on(selectedProductButton)
+            );
+        }
+
         actor.attemptsTo(
-
-                WaitUntil.the(PurchaseProductsInterfaces.BACKPACK_BUTTON, isVisible()).forNoMoreThan(20).seconds(),
-                Click.on(PurchaseProductsInterfaces.BACKPACK_BUTTON),
-
-                WaitUntil.the(PurchaseProductsInterfaces.BIKE_LIGHTS_BUTTON, isVisible()).forNoMoreThan(20).seconds(),
-                Click.on(PurchaseProductsInterfaces.BIKE_LIGHTS_BUTTON),
-
                 Click.on(PurchaseProductsInterfaces.SHOPPING_CART_CONTAINER)
         );
     }
--- src/test/java/org/example/userInterfaces/PurchaseProductsInterfaces.java
+++ src/test/java/org/example/userInterfaces/PurchaseProductsInterfaces.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 public class PurchaseProductsInterfaces {
     public static By BACKPACK_BUTTON = By.id("add-to-cart-sauce-labs-backpack");
     public static By BIKE_LIGHTS_BUTTON = By.id("add-to-cart-sauce-labs-bike-light");
+    public static By BOLT_TSHIRT_BUTTON = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
+    public static By FLEECE_JACKET_BUTTON = By.id("add-to-cart-sauce-labs-fleece-jacket");
+    public static By ONESIE_BUTTON = By.id("add-to-cart-sauce-labs-onesie");
+    public static By TSHIRT_RED_BUTTON = By.id("add-to-cart-test.allthethings()-t-shirt-(red)");
     public static By SHOPPING_CART_CONTAINER = By.id("shopping_cart_container");
     public static By CHECKOUT_BUTTON = By.id("checkout");
     public static By FINISH_BUTTON = By.id("finish");
